e129829aba47b2bf82726ca22f2973ab9395ea5d
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-09-19 13:22:59 +0200

    feat: added default controller for Products

---
+++ PiggyBank/PiggyBankMVC/Controllers/ProductsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PiggyBankMVC.DataAccessLayer;
+using SchemaBuilder.models;
+
+namespace PiggyBankMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly PiggyContext _context;
+
+        public ProductsController(PiggyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            var piggyContext = _context.products.Include(p => p.Manufacturer);
+            return View(await piggyContext.ToListAsync());
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.products
+                .Include(p => p.Manufacturer)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            ViewData["ManufacturerId"] = new SelectList(_context.manufacturers, "ManufacturerId", "Name");
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,Name,ImageUrl,Description,Height,Width,Length,Weight,Capacity,Color,Price,IsActive,ManufacturerId")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ManufacturerId"] = new SelectList(_context.manufacturers, "ManufacturerId", "Name", product.ManufacturerId);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["ManufacturerId"] = new SelectList(_context.manufacturers, "ManufacturerId", "Name", product.ManufacturerId);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,Name,ImageUrl,Description,Height,Width,Length,Weight,Capacity,Color,Price,IsActive,ManufacturerId")] Product product)
+        {
+            if (id != product.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ManufacturerId"] = new SelectList(_context.manufacturers, "ManufacturerId", "Name", product.ManufacturerId);
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.products
+                .Include(p => p.Manufacturer)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.products == null)
+            {
+                return Problem("Entity set 'PiggyContext.products'  is null.");
+            }
+            var product = await _context.products.FindAsync(id);
+            if (product != null)
+            {
+                _context.products.Remove(product);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+          return (_context.products?.Any(e => e.ProductId == id)).GetValueOrDefault();
+        }
+    }
+}
