76af8ebcf09aa06c32657e69921881349cc26638
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-09-21 14:29:29 +0200

    feat: Added FirstName & LastName fields on the Register form

--- PiggyBank/PiggyBankMVC/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PiggyBank/PiggyBankMVC/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,6 +71,15 @@ namespace PiggyBankMVC.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            [Display(Name = "First name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "Last name")]
+            public string Lastname { get; set; }
+
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -117,6 +126,10 @@ namespace PiggyBankMVC.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.Firstname = Input.FirstName;
+                user.Lastname = Input.Lastname;
+
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
