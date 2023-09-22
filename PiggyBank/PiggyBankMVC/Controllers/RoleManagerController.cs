bfb93c1c2677c99e4cc4a0863846352c10a8c4c8
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-09-22 16:15:51 +0200

    feat: added a page listing all existing roles

---
+++ PiggyBank/PiggyBankMVC/Controllers/RoleManagerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PiggyBankMVC.Controllers
+{
+    public class RoleManagerController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public RoleManagerController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var roles = _roleManager.Roles.ToList();
+            return View(roles);
+        }
+    }
+}
