077811b9efc3cfca472076775be61f09da7be12b
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-09-22 17:42:51 +0200

    feat: added a page listing all users and their roles feat: added a page allowing to edit a user's roles

---
+++ PiggyBank/PiggyBankMVC/Models/UserRolesManageViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PiggyBankMVC.Models
+{
+    public class UserRolesManageViewModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+        public bool Selected { get; set; }
+    }
+}
