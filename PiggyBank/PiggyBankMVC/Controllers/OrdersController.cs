80ce5a78f29062a70f1ff28d5c48c9c93bffad7a
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-09-26 16:40:27 +0200

    feat: added the list of OrderDetails on the Order details page

--- PiggyBank/PiggyBankMVC/Controllers/OrdersController.cs
+++ PiggyBank/PiggyBankMVC/Controllers/OrdersController.cs
@@ -50,7 +50,19 @@ namespace PiggyBankMVC.Controllers
                 return NotFound();
             }
 
-            return View(order);
+            var orderDetails = _context.OrderDetails
+                .Include(o => o.Product)
+                .Where(m => m.OrderId == id).ToList();
+
+            var vm = new OrderViewModel
+            {
+                Order = order,
+                Details = orderDetails,
+            };
+
+            Console.WriteLine(vm.Details);
+
+            return View(vm);
         }
 
         //// GET: Orders/Create
@@ -62,8 +74,6 @@ namespace PiggyBankMVC.Controllers
         //}
 
         //// POST: Orders/Create
-        //// To protect from overposting attacks, enable the specific properties you want to bind to.
-        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         //[HttpPost]
         //[ValidateAntiForgeryToken]
         //public async Task<IActionResult> Create([Bind("OrderId,CreatedAt,UserId,AddressId,OrderStatus")] Order order)
@@ -139,45 +149,6 @@ namespace PiggyBankMVC.Controllers
             ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", order.UserId);
             return View(order);
         }
-
-        //// GET: Orders/Delete/5
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    if (id == null || _context.Orders == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var order = await _context.Orders
-        //        .Include(o => o.Address)
-        //        .Include(o => o.User)
-        //        .FirstOrDefaultAsync(m => m.OrderId == id);
-        //    if (order == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(order);
-        //}
-
-        //// POST: Orders/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> DeleteConfirmed(int id)
-        //{
-        //    if (_context.Orders == null)
-        //    {
-        //        return Problem("Entity set 'PiggyContext.Orders'  is null.");
-        //    }
-        //    var order = await _context.Orders.FindAsync(id);
-        //    if (order != null)
-        //    {
-        //        _context.Orders.Remove(order);
-        //    }
-            
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction(nameof(Index));
-        //}
 
         private bool OrderExists(int id)
         {
---
+++ PiggyBank/PiggyBankMVC/Models/ViewModels/OrderViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PiggyBankMVC.Models.ViewModels
+{
+    public class OrderViewModel
+    {
+        public virtual Order? Order { get; set; }
+        public virtual List<OrderDetail>? Details { get; set; }
+    }
+}
