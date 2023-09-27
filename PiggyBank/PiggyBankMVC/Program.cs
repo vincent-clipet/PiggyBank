0ce039c7aad2a69e6524f6f31f899ed3c7109e8a
Vincent Clipet <vincent.clipet.7@gmail.com>
2023-09-27 09:40:45 +0200

    feat: made the auth cookie last for more than 5 minutes

--- PiggyBank/PiggyBankMVC/Program.cs
+++ PiggyBank/PiggyBankMVC/Program.cs
@@ -48,7 +48,7 @@ namespace PiggyBankMVC
             {
                 // Cookie settings
                 options.Cookie.HttpOnly = true;
-                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                options.ExpireTimeSpan = TimeSpan.FromDays(30);
 
                 options.LoginPath = "/Identity/Account/Login";
                 options.AccessDeniedPath = "/Identity/Account/AccessDenied";
